e3846a498b3a2bc2a915b5941d2addf9628f5965
dennis <40843235@gm.nfu.edu.tw>
2021-02-02 15:54:57 +0800

    deleting non-neccessary files

--- database/migrations/2021_01_25_082409_create_test_table.php
+++ database/migrations/2021_01_25_082409_create_test_table.php
@@ -15,8 +15,8 @@ class CreateTestTable extends Migration
     {
         Schema::create('test', function (Blueprint $table) {
             $table->id();
-	    $table->string('name');
-	    $table->string('email')->unique();
+			$table->string('name');
+			$table->string('email')->unique();
             $table->timestamps();
         });
     }
